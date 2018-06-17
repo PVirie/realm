d24ba37cd20b2985b03943cc5c4d0d39cb663d6f
Tao <tao@thebitstudio.com>
2018-06-17 10:21:03 +0700

    fixed defect in test

--- test.py
+++ test.py
@@ -10,14 +10,14 @@ def quadprog_solve_qp(Wx, b, sum_active, lamb):
     qp_G = (np.matmul(np.transpose(Wx), Wx) + np.identity(dim) - Wx - np.transpose(Wx)) + np.identity(dim) * lamb
     qp_a = (b - np.matmul(np.transpose(Wx), b))
 
-    qp_C = np.concatenate([np.ones([dim, 1]), np.identity(dim), np.identity(dim) * -1.0], axis=1)
-    qp_b = np.zeros([1 + dim + dim], dtype=np.float64)
-    qp_b[0] = sum_active
-    qp_b[(1 + dim):] = -2.0
+    qp_C = np.identity(dim)
+    qp_b = np.zeros([dim], dtype=np.float64)
+    # qp_b[0] = sum_active
+    # qp_b[(1 + dim):] = -2.0
 
     # print(qp_G.shape, qp_a.shape, qp_C.shape, qp_b.shape)
 
-    return np.maximum(quadprog.solve_qp(qp_G, qp_a, qp_C, qp_b, meq=1)[0], 0)
+    return np.maximum(quadprog.solve_qp(qp_G, qp_a, qp_C, qp_b, meq=0)[0], 0)
 
 
 def apply_gradients(gradients, delta, rate=0.001, name="adam"):
@@ -49,15 +49,15 @@ class AUC:
 
     def learn(self, session, x, learning_rate=0.01, iterations=20):
         for i in range(iterations):
-            __ = session.run((self.learn_op, self.objective), feed_dict={self.x: x, self.learning_rate: learning_rate})
-            print(__)
+            __ = session.run(self.learn_op, feed_dict={self.x: x, self.learning_rate: learning_rate})
+            print(__, i)
         return __
 
     def create_forward_graph(self, x):
         y = tf.nn.sigmoid(tf.matmul(self.w, x))
         x_ = tf.nn.sigmoid(tf.matmul(tf.transpose(self.w), y))
 
-        self.objective = tf.trace(tf.matmul(x - x_, tf.transpose(x - x_)))
+        self.objective = tf.trace(tf.matmul(x - x_, tf.transpose(x - x_))) / tf.cast(tf.reduce_prod(tf.shape(x)), tf.float32)
         grad = tf.gradients(self.objective, [self.w])
         learn_op = apply_gradients(zip(grad, [self.w]), self.objective, self.learning_rate)
 
@@ -70,17 +70,16 @@ if __name__ == '__main__':
     # data = np.concatenate([mnist.train.images, mnist.test.images, mnist.validation.images], axis=0)
     # labels = np.concatenate([mnist.train.labels, mnist.test.labels, mnist.validation.labels], axis=0)
 
-    y = np.transpose(1 - mnist.train.labels)
-    xt = mnist.train.images
+    y = np.transpose(1 - mnist.test.labels)
+    xt = mnist.test.images
     x = np.transpose(xt)
 
     sess = tf.Session()
     auc = AUC([28 * 28, 10])
     sess.run(tf.global_variables_initializer())
 
-    error = auc.learn(sess, x, 0.01, 10)
+    error = auc.learn(sess, x, 0.1, 100)
     h = auc.feedup(sess, x)
-    print(error, h.shape)
     h_ = auc.feedup(sess, np.transpose(mnist.test.images))
 
     ht = np.transpose(h)
@@ -93,19 +92,19 @@ if __name__ == '__main__':
     W = np.matmul(yht, np.transpose(np.linalg.pinv(np.transpose(hht))))
 
     r = np.matmul(W, h_)
-    r = np.argmin(r, axis=1)
+    r = np.argmin(r, axis=0)
     g = np.argmax(mnist.test.labels, axis=1)
-    print(np.sum(r == g))
+    print(np.sum(r == g) * 100 / h_.shape[1])
 
     # ----------------------- fixed point
 
     xa1 = np.concatenate([y * h, np.ones([1, y.shape[1]])], axis=0)
     axat = np.matmul(y, np.transpose(xa1))
     xaxat = np.matmul(xa1, np.transpose(xa1))
-    A = np.matmul(axat, np.transpose(np.linalg.pinv(np.transpose(xaxat))))
+    A = np.matmul(axat, np.transpose(np.linalg.pinv(np.transpose(xaxat + 0.0001 * np.identity(xa1.shape[0])))))
 
     count_correct = 0
-    for i in range(100):
+    for i in range(h_.shape[1]):
         Ax = np.matmul(A[:, 0:-1], np.diag(h_[:, i]))
         b = A[:, -1]
         r2 = quadprog_solve_qp(Ax, b, 9.0, 0.00001)
@@ -113,6 +112,6 @@ if __name__ == '__main__':
         if np.argmin(r2) == np.argmax(mnist.test.labels[i, :]):
             count_correct = count_correct + 1
 
-    print(count_correct)
+    print(count_correct * 100 / h_.shape[1])
 
     sess.close()
